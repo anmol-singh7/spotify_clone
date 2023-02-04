f08a8671d65bd221f8274759491aa1995274d30b
Anmol Singh <singhanmol0984@gmail.com>
2023-02-04 22:24:47 +0530

    make Design folder for all css class in which login.css handel classes for lofin page create a new file login.js for login page

---
+++ src/components/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../Designs/login.css"
+function Login() {
+    return (
+        <div className='login_body'>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div className='spotify-login-button'>Login</div></div>
+        </div >
+    )
+}
+export default Login;
